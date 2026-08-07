7d523cd4078ec0c8f8725946c4e8d215490499ec
Mathieu Imfeld <imfeldma@gmail.com>
2026-08-07 19:08:15 +0200

    Revert unrelated file formatting changes

--- src/loom/cli/main.py
+++ src/loom/cli/main.py
@@ -14,7 +14,6 @@ from loom.config import RootConfig
 
 console = rich.console.Console()
 
-
 async def config_list(config: RootConfig, args: argparse.Namespace) -> int:
     del args
     console.print(yaml.dump(config.model_dump()))
@@ -109,7 +108,6 @@ def _collect_overrides(args: argparse.Namespace) -> dict:
 
     return overrides
 
-
 async def main() -> int:
     try:
         parser = argparse.ArgumentParser(f'Loom {__version__}')
@@ -162,7 +160,6 @@ async def main() -> int:
 
 def run() -> int:
     return asyncio.run(main())
-
 
 if __name__ == '__main__':
     sys.exit(run())
--- src/loom/config/root_config.py
+++ src/loom/config/root_config.py
@@ -14,9 +14,7 @@ class RootConfig(BaseModel):
     Configuration of the kube-eng cluster
     """
 
-    config_path: pathlib.Path = Field(
-        description='The configuration file backing this object'
-    )
+    config_path: pathlib.Path = Field(description='The configuration file backing this object')
 
     @computed_field
     @property
@@ -35,12 +33,10 @@ class RootConfig(BaseModel):
         Returns:
             Nothing
         """
-        yaml.dump(
-            self.model_dump(
-                mode='json', exclude_none=True, exclude_computed_fields=True
-            ),
-            self.config_path.open('w'),
-        )
+        yaml.dump(self.model_dump(mode='json',
+                                  exclude_none=True,
+                                  exclude_computed_fields=True),
+                  self.config_path.open('w'))
 
     @classmethod
     def load(cls, config_path: pathlib.Path) -> 'RootConfig':
